dd568d76687c9d41be588228be1d590c3b101c64
Omar Chehab <omarchehab98@gmail.com>
2017-01-07 14:30:23 -0500

    package setup

---
+++ public/js/ts/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(React.createElement("p", null, "Hello World!"), document.getElementById('root'));
---
+++ src/ts/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <p>Hello World!</p>,
+  document.getElementById('root')
+);
