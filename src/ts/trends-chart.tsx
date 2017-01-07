25abcb04a868e584e39a1c9865c63face7682192
Dennis Tismenko <dTismenkoDeveloper@gmail.com>
2017-01-07 18:19:18 -0500

    created class layout for trends-chart

---
+++ src/ts/trends-chart.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as Chart from "react-chartjs-2";
+
+/**
+* Handles data parsing and displaying data for a specific trend.
+* @author Dennis Tismenko
+*/
+const BarChart = Chart.Bar;
+var data = 4;
+
+export class TrendsChart extends React.Component {
+  render() {
+    return (
+    <BarChart data={data} options={options} />);
+  }
+}
