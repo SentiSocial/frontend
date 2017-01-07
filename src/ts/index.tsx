23ceda17bc7b92c5176de0582a79dd6ad7b6333e
Omar Chehab <omarchehab98@gmail.com>
2017-01-07 16:06:27 -0500

    progress

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {NetworkBus} from './network-bus';
 
 ReactDOM.render(
   <p>Hello World!</p>,
   document.getElementById('root')
 );
+
+
+NetworkBus.getTrends(response => {
+  console.log(response);
+});
+NetworkBus.getSpecificTrends(response => {
+  console.log(response);
+}, 0);
+NetworkBus.getContent(response => {
+  console.log(response);
+}, 0);
+NetworkBus.getSpecificContent(response => {
+  console.log(response);
+}, 0, 0);
--- src/ts/network-bus.tsx
+++ src/ts/network-bus.tsx
@@ -1,2 +1,120 @@
 import {Promise} from 'promise-polyfill';
 import {fetch} from 'whatwg-fetch';
+
+const APIURL = 'https://rhysre.net/v1';
+const ENDPOINTS = {
+  trends: '/trends',
+  specificTrends: '/trends/{id}',
+  content: '/content?page={page}',
+  specificContent: '/content/{id}?page={page}'
+};
+/**
+ * NetworkBus class handles all communications with the REST API.
+ */
+export class NetworkBus {
+
+  static getTrends(callback: (response: TrendsPacket) => void) {
+    const endpoint = ENDPOINTS.trends;
+    const url = `${APIURL}${endpoint}`;
+    console.log('getTrends', url);
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        callback(response);
+      });
+  }
+
+  static getSpecificTrends(callback: (response: SpecificTrendsPacket) => void,
+    id: number) {
+    const endpoint = ENDPOINTS.specificTrends
+      .replace(/{id}/, `${id}`);
+    const url = `${APIURL}${endpoint}`;
+    console.log('getSpecificTrends', url);
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        callback(response);
+      });
+  }
+
+  static getContent(callback: (response: ContentPacket) => void,
+    page: number) {
+    const endpoint = ENDPOINTS.content
+      .replace(/{page}/, `${page}`);
+    const url = `${APIURL}${endpoint}`;
+    console.log('getContent', url);
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        callback(response);
+      });
+  }
+
+  static getSpecificContent(callback: (response: SpecificContentPacket) => void,
+    id: number, page: number) {
+    const endpoint = ENDPOINTS.specificContent
+      .replace(/{id}/, `${id}`)
+      .replace(/{page}/, `${page}`);
+    const url = `${APIURL}${endpoint}`;
+    console.log('getSpecificContent', url);
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        callback(response);
+      });
+  }
+}
+
+// Endpoint Interfaces
+
+export interface TrendsPacket {
+  id: number;
+  name: string;
+  sentiment: number;
+};
+
+export interface SpecificTrendsPacket {
+  name: string;
+  history: {
+    start: number;
+    end: number;
+    data: SpecificTrendsDataPacket[];
+  }
+};
+
+export interface ContentPacket {
+  news: NewsPacket[];
+  tweets: TweetPacket[];
+};
+
+export interface SpecificContentPacket {
+  news: NewsPacket[];
+  tweets: TweetPacket[];
+};
+
+// Interfaces used by Endpoint Interfaces
+
+export interface SpecificTrendsDataPacket {
+  sentiment: number;
+  volume: number;
+};
+
+export interface NewsPacket {
+  title: string;
+  timestamp: number; // unix
+  source: string; // url
+  media?: string; // url
+  description: string;
+};
+
+export interface TweetPacket {
+  id: number;
+};
