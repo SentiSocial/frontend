cc45121f30f0b59d1a99d325fb25eba210dfc08a
Omar Chehab <omarchehab98@gmail.com>
2017-01-07 19:44:48 -0500

    rendered news and tweets

---
+++ src/ts/page-trends.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import {NetworkBus, TrendPacket} from './network-bus';
+
+import {NewsComponent} from './news-component';
+import {TweetComponent} from './tweet-component';
+
+import {Trends} from './trends';
+import {News} from './news';
+import {Tweet} from './tweet';
+
+
+interface PageTrendsState {
+  trends?: Trends;
+  news?: News[];
+  tweets?: Tweet[];
+  remaining?: number;
+};
+
+export class PageTrends extends React.Component<undefined, PageTrendsState> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      trends: undefined,
+      news: [],
+      tweets: [],
+      remaining: -1,
+    };
+  }
+
+  componentWillMount() {
+    console.log('Component will mount');
+    NetworkBus.getTrends((err, response) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const trends = response;
+      this.setState({
+        trends: trends,
+      })
+    });
+
+    NetworkBus.getContent((err, response) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const newNews = response.news;
+      const newTweets = response.tweets;
+      const newRemaining = response.remaining;
+      this.setState(prevState => {
+        const news = prevState.news.concat(newNews);
+        const tweets = prevState.tweets.concat(newTweets);
+        return {
+          news: news,
+          tweets: tweets,
+          remaining: newRemaining,
+        };
+      })
+    }, 0);
+  }
+
+  render() {
+    return (
+      <div>
+        <main>
+          {this.state.news.map((news, i) =>
+            <NewsComponent key={i} news={news} />)}
+          {this.state.tweets.map(tweet =>
+            <TweetComponent key={tweet.id} tweet={tweet} />)}
+        </main>
+      </div>
+    );
+  }
+}
