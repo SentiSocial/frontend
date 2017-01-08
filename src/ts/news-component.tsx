59d61779fc195aff13e0532ed8eb4dae98ae8d9a
Omar Chehab <omarchehab98@gmail.com>
2017-01-07 21:23:02 -0500

    updated stuff

--- src/ts/news-component.tsx
+++ src/ts/news-component.tsx
@@ -1,6 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+const moment = require('moment');
+
+moment.updateLocale('en', {
+  relativeTime : {
+    future: "in %s",
+    past: "%s ago",
+    s: "seconds",
+    m: "a minute",
+    mm: "%d minutes",
+    h: "an hour",
+    hh: "%d hours",
+    d: "a day",
+    dd: "%d days",
+    M: "a month",
+    MM: "%d months",
+    y: "a year",
+    yy: "%d years",
+  }
+});
+
 import {News} from './news';
 
 interface NewsComponentProps {
@@ -15,11 +35,14 @@ export class NewsComponent extends
   }
 
   render() {
+    const source = this.props.news.source;
+    const time = moment(this.props.news.timestamp * 1000).fromNow();
+    const title = this.props.news.title;
     return (
       <div>
-        <h3>{this.props.news.source}</h3>
-        <span>{this.props.news.timestamp}</span>
-        <p>{this.props.news.title}</p>
+        <h3>{source}</h3>
+        <span>{time}</span>
+        <p>{title}</p>
       </div>
     );
   }
