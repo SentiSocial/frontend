b80bf8343c05a6a684bb0720aefeb022be00c421
Omar Chehab <omarchehab98@gmail.com>
2017-01-10 21:35:22 -0500

    created setup and webhook.php files in preparation for automatic deployment

---
+++ public/webhook.php
@@ -0,0 +1,32 @@
+<?php
+// Where is your deploy script located?
+$deploy = 'bash /home/ubuntu/deploy/deploy';
+// Get the secret token from the environment variables.
+$secret_token = getenv('WEBHOOK_SECRET_TOKEN');
+
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'POST':
+    $request_body = file_get_contents('php://input');
+    $payload = json_decode($request_body, JSON_OBJECT_AS_ARRAY);
+
+    $generated_signature = hash_hmac('sha1', $request_body, $secret_token);
+    $received_signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+    // Authenticating GitHub using the secret token.
+    if ($received_signature != $generated_signature) die;
+
+    // Which repository was pushed to?
+    $repository = $payload['repository']['name'];
+
+    // Which branch was pushed to?
+    $branch = array();
+    preg_match('/(?<=refs\/heads\/).+/i', $payload['ref'], $branch);
+    $branch = $branch[0];
+
+    switch ($branch) {
+      case 'master':
+        shell_exec("${deploy} '${repository}'");
+        break;
+    }
+    die;
+}
