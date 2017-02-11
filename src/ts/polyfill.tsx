615779a918905ab4d788204bfe57b9a9fbb20834
Omar Chehab <omarchehab98@gmail.com>
2017-02-11 17:01:49 -0500

    tested Helpers, seperated polyfill

---
+++ src/ts/polyfill.tsx
@@ -0,0 +1,30 @@
+import 'whatwg-fetch';
+import {Promise} from 'promise-polyfill';
+if (!window['Promise']) window['Promise'] = Promise;
+
+/**
+ * Object.assign polyfill by Mozilla
+ */
+if (typeof Object['assign'] !== 'function') {
+  Object['assign'] = function (target, varArgs) { // .length of function is 2
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    let to = Object(target);
+
+    for (let index = 1; index < arguments.length; index++) {
+      let nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (let nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
