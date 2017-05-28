d0d669da4f74713f4143ee7f47e3ab4522364f41
Omar Chehab <omarchehab98@gmail.com>
2017-05-28 20:13:28 +0300

    Move store.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import { h, Component } from 'preact'
 import { Router } from 'preact-router'
 import { Provider } from 'preact-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
 import './style'
 import '../lib/ga'
 import '../lib/twttr'
 
-import store from './store';
+import reducers from './reducers'
+
 import Navigation from './components/Navigation'
 import HomePage from './containers/HomePage';
 import TrendPage from './routes/TrendPage';
+
+createStore(
+  reducers, {
+    // Default application state
+    alltrends: undefined
+  },
+  applyMiddleware(
+    thunkMiddleware
+  )
+)
 
 export default class App extends Component {
   /** Gets fired when the route changes.
