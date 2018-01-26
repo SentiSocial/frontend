4d35e3f5e9b3b461bcf8502538757636bc034e40
Omar Chehab <omarchehab98@gmail.com>
2018-01-26 13:36:48 -0500

    Display error message when Tweet fails to load

--- src/components/TweetCard/index.js
+++ src/components/TweetCard/index.js
@@ -3,6 +3,10 @@ import style from './style.scss'
 const s = name => style[name] || name
 
 export default class TweetCard extends Component {
+  state = {
+    error: false,
+  }
+
   componentDidMount () {
     this.createTweetEmbed()
   }
@@ -17,6 +21,9 @@ export default class TweetCard extends Component {
     
     window.twttr.widgets.createTweetEmbed(embed_id, this.div, options)
       .then(this.restyleTweet)
+      .catch(error => this.setState({
+        error,
+      }))
   }
 
   restyleTweet = () => {
@@ -35,9 +42,20 @@ export default class TweetCard extends Component {
   }
 
   render () {
+    const {
+      error
+    } = this.state
     return (
       <div className={s('card') + ' ' + s('card--responsive')} style={{ padding: 0 }}>
         <div ref={div => { this.div = div }}/>
+        {error && (
+          <div style={{ padding: 15 }}>
+            <h3>Tweet Embed from Twitter</h3>
+            <p className={s('tweet--error')}>Tweet failed to load</p>
+            <p>Are you connected to the internet?</p>
+            <p>Perhaps a browser extension blocked it?</p>
+          </div>
+        )}
       </div>
     )
   }
