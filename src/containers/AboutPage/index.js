d9ba25e0de28a1cdcffbdd84869f028de4403e18
Omar Chehab <omarchehab98@gmail.com>
2018-01-26 16:01:51 -0500

    Update AboutPage styling
    Add attribution to NewsApi

--- src/containers/AboutPage/index.js
+++ src/containers/AboutPage/index.js
@@ -31,37 +31,52 @@ class AboutPage extends Component {
             Open source
           </h2>
 
-          <a href="https://github.com/SentiSocial">
-            https://github.com/SentiSocial
-          </a>
+          <div style={{ textAlign: 'center' }}>
+            <a href="https://github.com/SentiSocial" target="_blank" rel="noopener">
+              <img className={s('aboutpage--sentisociallogo')}
+                src="/assets/graphics/logo.png"
+                alt="SentiSocial Logo"
+                width="128"
+              />
+              <p>
+                <img className={s('aboutpage--githublogo')}
+                  src="/assets/graphics/github.svg"
+                  alt="GitHub Logo"
+                  width="16"
+                />
+                https://github.com/SentiSocial
+              </p>
+            </a>
 
-          <p>
-            <a className="github-button"
-              href="https://github.com/SentiSocial/frontend"
-              data-icon="octicon-star"
-              data-size="large"
-              data-show-count="true"
-              aria-label="Star SentiSocial/frontend on GitHub"
-              ref={el => { this.frontendStarButton = el }}
-            >
-              Frontend
-            </a>
-            &nbsp;
-            <a className="github-button"
-              href="https://github.com/SentiSocial/backend"
-              data-icon="octicon-star"
-              data-size="large"
-              data-show-count="true"
-              aria-label="Star SentiSocial/backend on GitHub"
-              ref={el => { this.backendStarButton = el }}
-            >
-              Backend
-            </a>
-          </p>
+            <p>
+              <a className="github-button"
+                href="https://github.com/SentiSocial/frontend"
+                data-icon="octicon-star"
+                data-size="large"
+                data-show-count="true"
+                aria-label="Star SentiSocial/frontend on GitHub"
+                ref={el => { this.frontendStarButton = el }}
+              >
+                Frontend
+              </a>
+              &nbsp;
+              <a className="github-button"
+                href="https://github.com/SentiSocial/backend"
+                data-icon="octicon-star"
+                data-size="large"
+                data-show-count="true"
+                aria-label="Star SentiSocial/backend on GitHub"
+                ref={el => { this.backendStarButton = el }}
+              >
+                Backend
+              </a>
+            </p>
+          </div>
 
           <h2 className={s('aboutpage--heading')}>
             Contributors
           </h2>
+
           <div className={s('aboutpage--contributors')}>
             {contributors.map(contributor => (
               <a
@@ -73,11 +88,19 @@ class AboutPage extends Component {
                 <img
                   src={contributor.avatar_url}
                   alt={contributor.login + ' Avatar'}
-                  width="128"
+                  width="64"
                 />
                 <span>{contributor.login}</span>
               </a>
             ))}
+          </div>
+
+          <h2 className={s('aboutpage--heading')}>
+            Attribution
+          </h2>
+
+          <div>
+            Powered by <a href="https://newsapi.org" target="_blank" rel="noopener">NewsApi</a>
           </div>
         </div>
       </div>
